77e470b3da80e37146f2146ecae4b3d9929f0c67
pjagada <pranavj918@gmail.com>
2021-10-13 00:46:57 -0400

    Populate angleOffsets.csv with every angle in the range to account for not found angles

--- generators/angleOffsets.py
+++ generators/angleOffsets.py
@@ -1,16 +1,40 @@
 import csv
 
+def rounded(n):
+    return str(round(n, 2))
+
+def roughlyEqual(a, b):
+    a = float(a)
+    if (a - b < 0.005):
+        return True
+    else:
+        return False
+
 data = []
 
-with open('rawOffsetSheet.csv', mode='r') as csvfile:
+with open('angleOffsets1.csv', mode='r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
-        #print(row)
-        shortRow = [row[i] for i in range(len(row)) if row[i] != '']
-        #print(shortRow)
-        data.append(shortRow)
+        data.append(row)
+
+
+print(len(data))
+#print(data[0:100])
+full = []
+full.append(data.pop(0))
+current = -179.88
+
+while(current <= 179.88):
+    if (data and roughlyEqual(data[0][0], current)):
+        full.append(data.pop(0))
+    else:
+        full.append([rounded(current)])
+    current += 0.01
+
 with open('angleOffsets.csv', mode='w', newline='') as file:
     fileWriter = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for j in range(len(data)):
-        dataRow = data[j]
+    for j in range(len(full)):
+        dataRow = full[j]
         fileWriter.writerow(dataRow)
+
+print(len(full))
---
+++ generators/angleOffsets1.py
@@ -0,0 +1,16 @@
+import csv
+
+data = []
+
+with open('rawOffsetSheet.csv', mode='r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        #print(row)
+        shortRow = [row[i] for i in range(len(row)) if row[i] != '']
+        #print(shortRow)
+        data.append(shortRow)
+with open('angleOffsets1.csv', mode='w', newline='') as file:
+    fileWriter = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for j in range(len(data)):
+        dataRow = data[j]
+        fileWriter.writerow(dataRow)
