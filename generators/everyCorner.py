6af8b6e44a5d8b4908e420f7669932480f545ce3
pjagada <pranavj918@gmail.com>
2021-11-29 01:33:35 -0500

    sort by angle and fill in empty angles

--- generators/everyCorner.py
+++ generators/everyCorner.py
@@ -1,11 +1,11 @@
 from itertools import product,chain
 import csv
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 from math import atan,degrees,pi,sqrt
 import os
 import threading
 
-threads = 4 # 1, 2, 4, 8, or 16
+threads = 8 # 1, 2, 4, 8, or 16
 
 RANGE = 3200
 
@@ -15,35 +15,24 @@ def getAngles(dist,chunkX,chunkZ):
     for x,z in product(range(-dist//16,dist//16 + 1),repeat=2):
         angle = round(degrees((-1+2*int(x<0)) * abs((pi if z < 0 else 0) - atan(abs((x+0.5-(chunkX/16))/(z+0.5-(chunkZ/16)))))),2)
         angles[angle].append((x,z))
-    return angles
-
-def ascendSort(oldList):
-    newList = []
-    while (oldList != []):
-        minIdx = -1
-        minDist = 99999
-        for i in range(len(oldList)):
-            dist = sqrt((oldList[i][0] ** 2) + (oldList[i][1] ** 2))
-            if (dist < minDist):
-                minIdx = i
-                minDist = dist
-        theTuple = oldList.pop(minIdx)
-        newList.append(theTuple)
-    return newList
+    for i in range(-17989, 17989):
+        missed_angle = i/100
+        if missed_angle not in angles:
+            angles[missed_angle] = []
+    sorted_angles = OrderedDict(sorted(angles.items(), key=lambda x: x[0]))
+    return sorted_angles
 
 def doit(q):
     q = int(q)
     print(f"starting thread {q} to {q+(16//threads)}")
-    data = [(x,z,getAngles(RANGE,x,z)) for x,z in chain.from_iterable([[(x1+0.3,z1+0.3),(x1+0.3,z1+0.7),(x1+0.7,z1+0.3),(x1+0.7,z1+0.7)] for x1,z1 in product(range(q, q+(16//threads)),range(0,16))])]
-    print("build data")
-    total = len(data)
+    total = 1024 / threads
     completed = set()
+    tablesDone = 0
+    for m in range(16):
+        data = [(x,z,getAngles(RANGE,x,z)) for x,z in chain.from_iterable([[(x1+0.3,z1+0.3),(x1+0.3,z1+0.7),(x1+0.7,z1+0.3),(x1+0.7,z1+0.7)] for x1,z1 in product(range(q, q+(16//threads)),range(m,m+1))])]
+        print("build data")
 
-    for i in range(len(data)):
-        percent = int(i * 100 / total)
-        if (percent not in completed):
-            completed.add(percent)
-            print(f"{percent}% done")
+        for i in range(len(data)):
             standX = str(data[i][0])
             standZ = str(data[i][1])
             file = "offsets\\" + "x" + standX + "z" + standZ + ".csv"
@@ -51,8 +40,13 @@ def doit(q):
                 writer = csv.writer(csvfile,quoting=csv.QUOTE_ALL)
                 angles = data[i]
                 for key,value in angles[2].items():
-                    newList = ascendSort(value)
+                    newList = sorted(value, key=lambda x: x[0]**2+x[1]**2)
                     writer.writerow([key,*[f"{x} {z}" for x,z in newList]])
+            tablesDone += 1
+            percent = int(tablesDone * 100 / total)
+            if (percent not in completed):
+                completed.add(percent)
+                print(f"{percent}% done with thread {int(q / (16 / threads))}")
     print(f"finishing thread {q} to {q+(16//threads)}")
 
 assert(threads in [1,2,4,8,16]), "Can only use 1, 2, 4, 8, or 16 threads"
@@ -68,5 +62,6 @@ for t1 in threadList:
 
 for t2 in threadList:
     t2.join()
+
 
 print("everything done")
